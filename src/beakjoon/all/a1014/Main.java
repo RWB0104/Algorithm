d0bd69cbe396cba7cf04c99dd3459bfde7021295
RWB0104 <psj1789@naver.com>
2021-06-15 20:23:31 +0900

    소스 갱신

--- src/beakjoon/all/a1014/Main.java
+++ src/beakjoon/all/a1014/Main.java
@@ -1,65 +1,49 @@
 package beakjoon.all.a1014;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 
-/**
- * 백준 전체 1014 문제 알고리즘 클래스
- *
- * @author RWB
- * @see <a href="https://rwb0104.github.io/posts/2021/06/13/A1014/">1014 풀이</a>
- * @since 2021.06.13 Sun 16:34:19
- */
 public class Main
 {
-	/**
-	 * 메인 함수
-	 *
-	 * @param args: [String[]] 매개변수
-	 *
-	 * @throws IOException 데이터 입출력 예외
-	 */
+	static int[][] room;
+	
+	static boolean[][] canSit;
+	
+	static int N, M;
+	
 	public static void main(String[] args) throws IOException
 	{
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		// 케이스 수
 		int T = Integer.parseInt(reader.readLine());
 		
-		for (int i = 0; i < T; i++)
+		while (T-- > 0)
 		{
 			String[] temp = reader.readLine().split(" ");
 			
-			// 교실 세로 길이
-			int N = Integer.parseInt(temp[0]);
+			N = Integer.parseInt(temp[0]);
+			M = Integer.parseInt(temp[1]);
 			
-			// 교실 가로 길이
-			int M = Integer.parseInt(temp[1]);
+			room = new int[N][M];
 			
-			int[][] seat = new int[N][M];
+			canSit = new boolean[N][M];
 			
-			for (int j = 0; j < N; j++)
+			for (int n = 0; n < N; n++)
 			{
+				Arrays.fill(room[n], 0);
+				
 				temp = reader.readLine().split("");
 				
-				for (int k = 0; k < temp.length; k++)
+				for (int m = 0; m < temp.length; m++)
 				{
-					seat[j][k] = temp[k].equals(".") ? 1 : 0;
+					canSit[n][m] = temp[m].equals(".");
 				}
-			}
-			
-			for (int j = 0; j < N; j++)
-			{
-				for (int k = 0; k < temp.length; k++)
-				{
-					System.out.print(seat[j][k]);
-				}
-				
-				System.out.println();
 			}
 		}
 		
+		writer.flush();
+		writer.close();
 		reader.close();
 	}
 }
